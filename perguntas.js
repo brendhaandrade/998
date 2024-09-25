0046107c9a7d509e5491420b20688b9636e93228
brendhaandrade <148974947+brendhaandrade@users.noreply.github.com>
2024-09-24 22:27:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'educação fisica',
+    'em qual ano surgiu o futebol?',
+    'em 1863'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'Buenos Aires'
+)
+
+criaCartao(
+    'Arte',
+    'quando surgiu a capoeira?',
+    ' século XVII(17)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz qual o seu nome em ingles?',
+    'what is your name'
+)
